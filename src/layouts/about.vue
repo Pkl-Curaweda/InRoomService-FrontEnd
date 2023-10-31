c1b5b3de55147c50e6a7c071b5e7ef6e5ab6d377
Nadif Dzaikra <nadifdzaikra@gmail.com>
2023-10-31 12:36:18 +0700

    add component & create layout

---
+++ src/layouts/about.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="q-pa-md">
+    <!-- <q-layout view="lHh lpr lFf" class="shadow-2 rounded-borders">
+      <q-header elevated :class="$q.dark.isActive ? 'bg-secondary' : 'bg-black'">
+        <q-toolbar>
+          <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
+          <q-toolbar-title>Header</q-toolbar-title>
+        </q-toolbar>
+      </q-header>
+    </q-layout> -->
+    <q-layout view="lHh lpR fFf">
+      <q-header reveal class="bg-[#069550] text-white">
+        <q-toolbar>
+          <q-toolbar-title class="text-capitalize font-semibold">
+            <!-- {{ route.meta.label }} -->
+            <q-avatar>
+              <img src="../assets/img/lingian-logo-colored.png" alt="" class="opacity-100" />
+            </q-avatar>
+          </q-toolbar-title>
+
+          <router-link to="/">
+            <q-btn flat round icon="o_mail" class="opacity-80 hover:opacity-100" />
+          </router-link>
+
+          <router-link to="/">
+            <q-btn flat round icon="o_account_circle" class="opacity-80 hover:opacity-100" />
+          </router-link>
+        </q-toolbar>
+      </q-header>
+    </q-layout>
+  </div>
+</template>
+
+<script setup>
+  import { useRoute, useRouter } from 'vue-router'
+  const route = useRoute()
+</script>
+
+<style lang="scss" scoped></style>
