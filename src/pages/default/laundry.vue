1dcb99f0774d861eb99c5993778aac21b853b767
Nadif Dzaikra <nadifdzaikra@gmail.com>
2023-10-31 09:41:32 +0700

    tes

--- src/pages/default/laundry.vue
+++ src/pages/default/laundry.vue
@@ -1,5 +1,11 @@
 <template>
-  <Receipt />
+  <Receipt
+    items="Betadine"
+    totalitems="2"
+    subtotal="2000"
+    ppn="1000"
+    servicefees="200"
+    total="3200" />
 </template>
 
 <script setup></script>
