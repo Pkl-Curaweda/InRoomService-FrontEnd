76ae50f2bce9c876c3a083c5d2ff0270b7b6738b
Nadif Dzaikra <nadifdzaikra@gmail.com>
2023-11-22 15:04:36 +0700

    fetching api foodbeverage

--- src/pages/default/foodbeverage.vue
+++ src/pages/default/foodbeverage.vue
@@ -1,7 +1,8 @@
 <script lang="ts">
+  import axios from 'axios'
   import CardUser from 'src/components/CardUser.vue'
   import { defineProps, ref, defineEmits, toRef } from 'vue'
-
+  import api from 'src/AxiosInterceptors'
   export default {
     components: {
       CardUser,
@@ -9,11 +10,6 @@
 
     data() {
       return {
-        cartData: {
-          namaProduk: String,
-          hargaProduk: Number,
-          qty: Number,
-        },
         cart: [] as {
           namaProduk: string
           hargaProduk: number
@@ -22,32 +18,36 @@
         }[],
         cardData: [
           {
-            gambarProduk: 'desdelux.png',
-            namaProduk: 'Bakso Ngawi',
-            descProduk: 'Dibuat dari daging unta asli baghdad',
-            hargaProduk: 20000,
-          },
-          {
-            gambarProduk: 'desdelux.png',
+            gambarProduk: 'crash.jpg',
             namaProduk: 'Bakso Baghdad',
             descProduk: 'Dibuat dari daging unta asli baghdad',
             hargaProduk: 20000,
           },
           {
-            gambarProduk: 'desdelux.png',
+            gambarProduk: 'crash.jpg',
             namaProduk: 'Bakso Bogor',
             descProduk: 'Dibuat dari daging unta asli baghdad',
             hargaProduk: 20000,
           },
           {
-            gambarProduk: 'desdelux.png',
+            gambarProduk: 'crash.jpg',
             namaProduk: 'Bakso Bagong',
+            descProduk: 'Dibuat dari daging unta asli baghdad',
+            hargaProduk: 20000,
+          },
+          {
+            gambarProduk: 'crash.jpg',
+            namaProduk: 'Bakso Baghdad',
             descProduk: 'Dibuat dari daging unta asli baghdad',
             hargaProduk: 20000,
           },
         ],
         price: 0,
+        data: [] as { name: string; price: number; desc: string; picture: string }[],
       }
+    },
+    mounted() {
+      this.getDataFromApi()
     },
     setup(props, { emit }) {
       const count = ref(0)
@@ -73,58 +73,44 @@
       }
     },
     methods: {
+      async getDataFromApi() {
+        try {
+          const response = await api.get('/services/2', {
+            withCredentials: true,
+          })
+          console.log('Response status:', response.status)
+          console.log('Response data:', response.data)
+          console.log(response.data)
+          this.data = response.data.data
+        } catch (error) {
+          console.error('Error fetching data:', error)
+        }
+      },
       updateTotalPrice(value: any) {
         console.log(value)
         this.price = value
         this.$emit('total', value)
       },
 
-      addToCart(card: any) {
-        const existingProductIndex = this.cart.findIndex(
-          (item) => item.namaProduk === card.namaProduk
-        )
+      saveCartToLocalStorage() {
+        localStorage.setItem('cart', JSON.stringify(this.cart))
+      },
 
-        if (existingProductIndex === -1) {
-          // If the product is not in the cart, add a new entry
+      addToCart(card: any) {
+        const existingProduct = this.cart.find((item) => item.namaProduk === card.namaProduk)
+
+        if (!existingProduct) {
           this.cart.push({
             namaProduk: card.namaProduk,
             hargaProduk: card.hargaProduk,
             gambarProduk: card.gambarProduk,
             qty: 1,
           })
-        } else {
-          // If the product is already in the cart, update the quantity
-          this.cart[existingProductIndex].qty += 1
-        }
 
-        // Save the updated cart to localStorage
-        this.saveCartToLocalStorage()
+          this.saveCartToLocalStorage()
+        }
 
         console.log(this.cart)
-      },
-
-      saveCartToLocalStorage() {
-        // Retrieve the existing cart from localStorage
-        const existingCart = JSON.parse(localStorage.getItem('cartFood') || '[]')
-
-        // Update the existing cart with the current cart from the component
-        for (const newItem of this.cart) {
-          const existingProductIndex = existingCart.findIndex(
-            (item: { namaProduk: string }) => item.namaProduk === newItem.namaProduk
-          )
-
-          if (existingProductIndex === -1) {
-            // If the product is not in the existing cart, add a new entry
-            existingCart.push({ ...newItem })
-          } else {
-            // If the product is already in the existing cart, update the quantity
-            // existingCart[existingProductIndex].qty += newItem.qty
-            console.log('item sudah ada')
-          }
-        }
-
-        // Save the updated cart to localStorage
-        localStorage.setItem('cartFood', JSON.stringify(existingCart))
       },
 
       increment(card: any) {
@@ -184,61 +170,61 @@
 </script>
 
 <template>
-  <div class="h-full overflow-scroll">
+  <div class="h-full overflow-y-scroll scrollhide justify-center items-center mt-5">
     <div class="flex flex-col gap-4 items-center">
       <p class="hidden">{{ subTotal }}</p>
-      <div v-for="(card, index) in cardData" :key="index" class="mx-auto w-screen px-5">
+      <div v-for="(card, index) in data" :key="index" class="mx-auto w-screen px-5">
         <CardUser
-          :gambarProduk="`/src/assets/img/${card.gambarProduk}`"
-          :namaProduk="card.namaProduk"
-          :descProduk="card.descProduk"
-          :hargaProduk="card.hargaProduk"
+          :gambarProduk="`${card.picture}`"
+          :namaProduk="card.name"
+          :descProduk="card.desc"
+          :hargaProduk="card.price"
           @quantityChanged="updateTotalPrice"
           :onClick="() => addToCart(card)"
           class="mx-auto" />
 
-        <!-- <q-card class="card my-card text-white p-3 w-[380px]">
-          <q-card-section horizontal class="flex justify-between">
-            <div class="tulisan">
-              <div class="text-md pb-1 font-bold">{{ card.namaProduk }}</div>
-              <div class="text-xs text-justify">
-                {{ card.descProduk }}
-              </div>
-              <div class="flex flex-row items-center justify-between">
-                <div class="text-sm">
-                  {{ card.hargaProduk }}
+        <!-- <q-card class="card my-card text-white p-3">
+            <q-card-section horizontal class="flex justify-around">
+              <div class="tulisan my-auto">
+                <div class="text-md pb-1 font-bold">{{ card.namaProduk }}</div>
+                <div class="text-xs text-justify">
+                  {{ card.descProduk }}
+                </div>
+                <div class="flex flex-row items-center justify-between">
+                  <div class="text-sm">
+                    {{ card.hargaProduk }}
+                  </div>
+
+                  <q-card-actions>
+                    <q-btn
+                      @click="decrement(card)"
+                      class="border-[#16A75C] text-black border-2"
+                      round
+                      color="white"
+                      icon="remove"
+                      size="sm" />
+
+                    <p class="mx-1 px-2">{{ getQuantity(card) }}</p>
+                    <q-btn
+                      @click="increment(card)"
+                      class="border-[#16A75C] text-black border-2"
+                      round
+                      color="white"
+                      icon="add"
+                      size="sm" />
+                  </q-card-actions>
                 </div>
                 <q-card-actions>
                   <q-btn
-                    @click="decrement(card)"
-                    class="border-[#16A75C] text-black border-2"
-                    round
-                    color="white"
-                    icon="remove"
-                    size="sm" />
-
-                  <p class="mx-1 px-2">{{ getQuantity(card) }}</p>
-                  <q-btn
-                    @click="increment(card)"
-                    class="border-[#16A75C] text-black border-2"
-                    round
-                    color="white"
-                    icon="add"
-                    size="sm" />
+                    class="bg-green w-32 rounded-full text-sm text-black font-bold"
+                    label="Send"
+                    name="send"
+                    @click="addToCart(card)" />
                 </q-card-actions>
               </div>
-            </div>
-            <q-img :src="`/src/assets/img/${card.gambarProduk}`" class="col-4" ratio="1" />
-          </q-card-section>
-
-          <q-card-actions>
-            <q-btn
-              class="bg-green w-32 rounded-full text-sm text-black font-bold"
-              label="Send"
-              name="send"
-              @click="addToCart(card)" />
-          </q-card-actions>
-        </q-card> -->
+              <q-img :src="`/src/assets/img/${card.gambarProduk}`" class="col-4" ratio="1" />
+            </q-card-section>
+          </q-card> -->
       </div>
     </div>
   </div>
@@ -247,5 +233,9 @@
 <style scoped>
   .card {
     background-color: rgba(153, 153, 153, 0.92);
+  }
+
+  .scrollhide::-webkit-scrollbar {
+    width: 0px;
   }
 </style>
