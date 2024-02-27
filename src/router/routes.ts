f240b027b33368417437ac6e062f938566210b29
Nadif Dzaikra <nadifdzaikra@gmail.com>
2024-02-27 09:26:19 +0700

    Set

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -7,7 +7,12 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [{ path: '', component: () => import('pages/IndexPage.vue'),
+    meta: {
+      title: 'Dashboard',
+      protected: true
+    }
+  }],
   },
   {
     path: '/',
